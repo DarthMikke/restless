919230b0c2e9ffad5dcbba2fe771eb350e3dfc47
Michal Jan Warecki <mwarecki1@gmail.com>
2024-03-23 20:37:47 +0100

    get_permalink

--- urls.py
+++ urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', FrontPage.as_view()),
     path('api/index', index_api),
     path('rss.xml', get_rss),
-    path('<int:year>/<int:month>/<int:day>/<str:name>', get_post),
+    path('blog/<str:name>', PostView.as_view(), name='post'),
     path('<int:year>/<int:month>/<int:day>/<str:name>/edit', edit_post),
     path('posts/<int:postid>', get_post_by_id),
-    path('<str:name>', PageView.as_view()),
+    path('<str:name>', PageView.as_view(), name='page'),
     #path('media/<int:resourceid>', get_resource),
 ]
